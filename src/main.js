87485d2ab111a930a6e2bd02a9790eaaa93fb4d8
bhauptvogel <b.vah317@gmail.com>
2025-05-09 01:54:46 +0200

    geohash added but not finished

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import VectorLayer from 'ol/layer/Vector.js';
 import OSM from 'ol/source/OSM.js';
 import Polygon from 'ol/geom/Polygon.js';
 import Feature from 'ol/Feature.js'
+import {toLonLat, fromLonLat} from 'ol/proj.js';
 import VectorSource from 'ol/source/Vector.js';
 import {Circle, Fill, Stroke, Style} from 'ol/style.js';
+import geohash from 'ngeohash';
 
 const tileStyle = new Style({
   stroke: new Stroke({
@@ -79,8 +81,8 @@ function drawCurrentSlippyTiles() {
 
 	const tilesToDraw = [];
 	 
-	const min = coordinatesToSlippyTile(zoom, viewExtent[0], viewExtent[3], zoom); // Top-left corner
-	const max = coordinatesToSlippyTile(zoom, viewExtent[2], viewExtent[1], zoom); // Bottom-right corner
+	const min = coordinatesToSlippyTile(zoom, viewExtent[0], viewExtent[3]); // Top-left corner
+	const max = coordinatesToSlippyTile(zoom, viewExtent[2], viewExtent[1]); // Bottom-right corner
 	for (let x = min.xTile; x < max.xTile+1; x++) {
 		for (let y = min.yTile; y < max.yTile+1; y++) {
 			const f = new Feature(new Polygon(tileBounds(x, y)));
@@ -98,20 +100,84 @@ function getSlippyTileID(coordinates) {
 	console.log(`ID: ${zoom}/${tileCoordinates.xTile}/${tileCoordinates.yTile}`);
 }
 
-// initial draw
-drawCurrentSlippyTiles();
+const base32 = '0145hjnp2367km1r89destwxbcfguvyz'; // (geohash-specific) Base32 map
+
+function getGeoHashTiles(sw, ne, precision) {
+
+  const lastBitSW = sw.charAt(sw.length-1);
+	const lastBitNE = ne.charAt(ne.length-1);
+
+	const evenBit = sw % 2 == 1;
+	const divider = evenBit ? 4 : 8;
+
+	const swIdx = base32.indexOf(lastBitSW);
+	const neIdx = base32.indexOf(lastBitNE);
+	const swPos = [Math.floor(swIdx/divider),swIdx%divider];
+	const nePos = [Math.floor(neIdx/divider),neIdx%divider];
+	const diff = [nePos[0]-swPos[0],nePos[1]-swPos[1]];
+
+	const tiles = [];
+	for (let x = 0; x < diff[0] + 1; x++) {
+		for (let y = 0; y < diff[1] + 1; y++) {
+			let [boxMinLat, boxMinLon, boxMaxLat, boxMaxLon] = geohash.decode_bbox(geohash.neighbor(sw,[x,y]));
+
+			let bottomLeft = fromLonLat([boxMinLon, boxMinLat]);
+			let bottomRight = fromLonLat([boxMaxLon, boxMinLat]);
+			let topRight = fromLonLat([boxMaxLon, boxMaxLat]);
+			let topLeft = fromLonLat([boxMinLon, boxMaxLat]);
+
+			tiles.push([
+				bottomLeft,
+				bottomRight,
+				topRight,
+				topLeft,
+				bottomLeft, // close ring
+			])
+		}
+	}
+  return tiles;
+}
+
+
+function drawCurrentGeohashTiles() {
+
+	vectorSource.clear();
+
+	const viewExtent = view.calculateExtent(map.getSize());
+	let sw = toLonLat([viewExtent[0], viewExtent[1]]);
+	sw = geohash.encode(sw[1], sw[0], 12);
+	let ne = toLonLat([viewExtent[2], viewExtent[3]]);
+	ne = geohash.encode(ne[1], ne[0], 12);
+	let index = 0;
+	while (index < sw.length && sw[index] === ne[index]) index++;
+	sw = sw.slice(0, index+1);
+	ne = ne.slice(0, index+1);
+	const precision = index + 1;
+	const tiles = getGeoHashTiles(sw, ne, precision);
+	const features = tiles.map(tile => new Feature(new Polygon([tile])));
+	features.forEach(feature => feature.setStyle(tileStyle));
+
+	vectorSource.addFeatures(features);
+}
+
+
+const drawMode = 'Slippy';
+
 
 // zoom change
 map.getView().on('change:resolution', () => {
-	drawCurrentSlippyTiles();
+	if (drawMode == 'GeoHash') drawCurrentGeohashTiles();
+	else if(drawMode == 'Slippy') drawCurrentSlippyTiles();
 });
 
 // map moving
-// TODO: not just on move end
+// TODO: not just on move end (but all the time when moving, continuous)
 map.on('moveend', () => {
-	drawCurrentSlippyTiles();
+	if (drawMode == 'GeoHash') drawCurrentGeohashTiles();
+	else if(drawMode == 'Slippy') drawCurrentSlippyTiles();
 });
 
 map.on('pointermove', function (event) {
-	getSlippyTileID(event.coordinate);
+	if (drawMode == 'GeoHash') console.log(geohash.encode(toLonLat(event.coordinate)[1], toLonLat(event.coordinate)[0], 1));
+	else if(drawMode == 'Slippy') getSlippyTileID(event.coordinate);
 });
