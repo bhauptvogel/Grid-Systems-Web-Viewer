6befaefc5c92255bb04c51572976417ec85c9f97
bhauptvogel <b.vah317@gmail.com>
2025-05-10 21:48:35 +0200

    fixing geohash rendering and refactoring

--- src/main.js
+++ src/main.js
@@ -41,143 +41,158 @@ const map = new Map({
 	view: view,
 });
 
+function cornersToRectanglePolygon(minX, maxX, minY, maxY) { return [[[minX, minY],[minX,maxY],[maxX,maxY],[maxX,minY],[minX,minY]]] }
+
 const worldRadius = 6378137;
 const originShift = Math.PI * worldRadius;
 
-function coordinatesToSlippyTile(zoom, x, y) {
-	const tileCount = Math.pow(2,zoom);
-	const resolution = (originShift * 2) / tileCount;
+class SlippyTilesGrid {
+	#zoom() {
+		return Math.max(3, Math.floor(map.getView().getZoom()+0.5));
+	}
+	#getTile(coordinates) {
+		const tileCount = Math.pow(2,this.#zoom());
+		const resolution = (originShift * 2) / tileCount;
 
-	const xTile = Math.floor((x + originShift) / resolution);
-	const yTile = Math.floor((originShift - y) / resolution);
+		const xTile = Math.floor((coordinates[0] + originShift) / resolution);
+		const yTile = Math.floor((originShift - coordinates[1]) / resolution);
 
-	return { xTile, yTile };
-}
+		return { xTile, yTile };
+	}
+	gridPolygons() {
+		const polygons = [];
 
-function drawCurrentSlippyTiles() {
-	const view = map.getView();
-	const zoom = Math.floor(view.getZoom());
-	const tileCount = Math.pow(2,zoom);
-	const viewExtent = view.calculateExtent(map.getSize());
-	
-	function tileBounds(x, y) {
-		// TODO Refactor: use coordinatesToSlippyTile function
+		const view = map.getView();
+		const viewExtent = view.calculateExtent(map.getSize());
+		const tileCount = Math.pow(2,this.#zoom());
 		const resolution = (2 * originShift) / tileCount;
-
-		const minX = -originShift + x * resolution;
-		const maxX = -originShift + (x + 1) * resolution;
-
-		const maxY = originShift - y * resolution;
-		const minY = originShift - (y + 1) * resolution;
-
-		return [[
-			[minX, minY],
-			[minX, maxY],
-			[maxX, maxY],
-			[maxX, minY],
-			[minX, minY]
-		]];
-	}
-
-	const tilesToDraw = [];
-	 
-	const min = coordinatesToSlippyTile(zoom, viewExtent[0], viewExtent[3]); // Top-left corner
-	const max = coordinatesToSlippyTile(zoom, viewExtent[2], viewExtent[1]); // Bottom-right corner
-	for (let x = min.xTile; x < max.xTile+1; x++) {
-		for (let y = min.yTile; y < max.yTile+1; y++) {
-			const f = new Feature(new Polygon(tileBounds(x, y)));
-			f.setStyle(tileStyle);
-			tilesToDraw.push(f);
+		 
+		// slippy tile definition: Northwest = [0,0]
+		const nwCoords = this.#getTile([viewExtent[0], viewExtent[3]]);
+		const seCoords = this.#getTile([viewExtent[2], viewExtent[1]]);
+		for (let x = nwCoords.xTile; x < seCoords.xTile+1; x++) {
+			for (let y = nwCoords.yTile; y < seCoords.yTile+1; y++) {
+				const minX = -originShift + x * resolution;
+				const maxX = -originShift + (x + 1) * resolution;
+				const maxY = originShift - y * resolution;
+				const minY = originShift - (y + 1) * resolution;
+				polygons.push(cornersToRectanglePolygon(minX, maxX, minY, maxY));
+			}
 		}
+		return polygons;
 	}
-	vectorSource.clear();
-	vectorSource.addFeatures(tilesToDraw);
+	getID(coordinates) {
+		const tile = this.#getTile(coordinates);
+		return `${this.#zoom()}/${tile.xTile}/${tile.yTile}`;
+	}
 }
 
-function getSlippyTileID(coordinates) {
-	const zoom = Math.floor(view.getZoom());
-	const tileCoordinates = coordinatesToSlippyTile(zoom, coordinates[0], coordinates[1]);
-	console.log(`ID: ${zoom}/${tileCoordinates.xTile}/${tileCoordinates.yTile}`);
-}
+const base32 = "0123456789bcdefghjkmnpqrstuvwxyz";
 
-const base32 = '0145hjnp2367km1r89destwxbcfguvyz'; // (geohash-specific) Base32 map
-
-function getGeoHashTiles(sw, ne, precision) {
-
-  const lastBitSW = sw.charAt(sw.length-1);
-	const lastBitNE = ne.charAt(ne.length-1);
-
-	const evenBit = sw % 2 == 1;
-	const divider = evenBit ? 4 : 8;
-
-	const swIdx = base32.indexOf(lastBitSW);
-	const neIdx = base32.indexOf(lastBitNE);
-	const swPos = [Math.floor(swIdx/divider),swIdx%divider];
-	const nePos = [Math.floor(neIdx/divider),neIdx%divider];
-	const diff = [nePos[0]-swPos[0],nePos[1]-swPos[1]];
-
-	const tiles = [];
-	for (let x = 0; x < diff[0] + 1; x++) {
-		for (let y = 0; y < diff[1] + 1; y++) {
-			let [boxMinLat, boxMinLon, boxMaxLat, boxMaxLon] = geohash.decode_bbox(geohash.neighbor(sw,[x,y]));
-
-			let bottomLeft = fromLonLat([boxMinLon, boxMinLat]);
-			let bottomRight = fromLonLat([boxMaxLon, boxMinLat]);
-			let topRight = fromLonLat([boxMaxLon, boxMaxLat]);
-			let topLeft = fromLonLat([boxMinLon, boxMaxLat]);
-
-			tiles.push([
-				bottomLeft,
-				bottomRight,
-				topRight,
-				topLeft,
-				bottomLeft, // close ring
-			])
+class GeohashGrid {
+	// # of characters of geohash (analogous to zoom)
+	#precision() { return 1 + Math.floor(map.getView().getZoom()/3); }
+	#charToPos(char, isEven) {
+		// AI generated (TODO check correctness!)
+		const idx = base32.indexOf(char);
+		if (idx === -1) throw new Error(`Invalid geohash character: ${char}`);
+		
+		let row = 0;
+		let col = 0;
+		for (const mask of [16, 8, 4, 2, 1]) {
+			const bit = (idx & mask) ? 1 : 0;
+			if (isEven) col = (col << 1) | bit;
+			else row = (row << 1) | bit;
+			isEven = !isEven;
 		}
+
+		return [col, row];
 	}
-  return tiles;
+	#innerPolygons(hash, differentIdx) {
+		const polygons = [];
+		for (let i = 1; i < base32.length; i++) {
+			const innerTileHash = hash + base32[i];
+			const bbox = geohash.decode_bbox(innerTileHash);
+			polygons.push([[
+				fromLonLat([bbox[1], bbox[0]]), // bottom-left
+				fromLonLat([bbox[3], bbox[0]]), // top-left
+				fromLonLat([bbox[3], bbox[2]]), // top-right
+				fromLonLat([bbox[1], bbox[2]]), // bottom-right
+				fromLonLat([bbox[1], bbox[0]]), // close the loop
+			]]);
+			if (this.#precision() > differentIdx + 1) polygons.push(...this.#innerPolygons(innerTileHash, differentIdx + 1));
+		}
+		return polygons;
+	}
+	gridPolygons() {
+		const polygons = [];
+
+		const view = map.getView();
+		const viewExtent = view.calculateExtent(map.getSize());
+		const precision = this.#precision();
+
+		const [swLon, swLat] = toLonLat([viewExtent[0],viewExtent[1]]);
+		const swHash = geohash.encode(swLat, swLon, precision);
+		const [neLon, neLat] = toLonLat([viewExtent[2],viewExtent[3]]);
+		const neHash = geohash.encode(neLat, neLon, precision);
+
+	  // # get geohash tiles
+		// first character that is different between sw and ne
+		let index = 0;
+		while (index < swHash.length && swHash[index] === neHash[index]) index++;
+		const firstDifferentIdx = index;
+
+		// draw *visible* tiles of inner layers
+		const swPos = this.#charToPos(swHash.charAt(firstDifferentIdx), firstDifferentIdx % 2 == 1);
+		const nePos = this.#charToPos(neHash.charAt(firstDifferentIdx), firstDifferentIdx % 2 == 1);
+		
+		for (let x = 0; x < (nePos[0]-swPos[0]) + 1; x++) {
+			for (let y = 0; y < (nePos[1]-swPos[1]) + 1; y++) {
+				const tileHash = geohash.neighbor(swHash.slice(0,firstDifferentIdx+1), [x, y]);
+				const bbox = geohash.decode_bbox(tileHash);
+				polygons.push([[
+					fromLonLat([bbox[1], bbox[0]]), // bottom-left
+					fromLonLat([bbox[3], bbox[0]]), // top-left
+					fromLonLat([bbox[3], bbox[2]]), // top-right
+					fromLonLat([bbox[1], bbox[2]]), // bottom-right
+					fromLonLat([bbox[1], bbox[0]]), // close the loop
+				]]);
+				if (precision > firstDifferentIdx+1) polygons.push(...this.#innerPolygons(tileHash, firstDifferentIdx+1));
+			}
+		}
+		return polygons;
+	}
+	getID(coordinates) {
+		return geohash.encode(toLonLat(coordinates)[1], toLonLat(coordinates)[0], this.#precision());
+	}
 }
 
 
-function drawCurrentGeohashTiles() {
+var gridSystem = new GeohashGrid();
 
+function drawGrid() {
 	vectorSource.clear();
-
-	const viewExtent = view.calculateExtent(map.getSize());
-	let sw = toLonLat([viewExtent[0], viewExtent[1]]);
-	sw = geohash.encode(sw[1], sw[0], 12);
-	let ne = toLonLat([viewExtent[2], viewExtent[3]]);
-	ne = geohash.encode(ne[1], ne[0], 12);
-	let index = 0;
-	while (index < sw.length && sw[index] === ne[index]) index++;
-	sw = sw.slice(0, index+1);
-	ne = ne.slice(0, index+1);
-	const precision = index + 1;
-	const tiles = getGeoHashTiles(sw, ne, precision);
-	const features = tiles.map(tile => new Feature(new Polygon([tile])));
+	const polygons = gridSystem.gridPolygons();
+	const features = polygons.map(polygon => new Feature(new Polygon(polygon)));
 	features.forEach(feature => feature.setStyle(tileStyle));
-
 	vectorSource.addFeatures(features);
 }
 
-
-const drawMode = 'Slippy';
-
+function logCoordinates(coordinates) {
+	console.log(gridSystem.getID(coordinates));
+}
 
 // zoom change
 map.getView().on('change:resolution', () => {
-	if (drawMode == 'GeoHash') drawCurrentGeohashTiles();
-	else if(drawMode == 'Slippy') drawCurrentSlippyTiles();
+	drawGrid();
 });
 
 // map moving
 // TODO: not just on move end (but all the time when moving, continuous)
 map.on('moveend', () => {
-	if (drawMode == 'GeoHash') drawCurrentGeohashTiles();
-	else if(drawMode == 'Slippy') drawCurrentSlippyTiles();
+	drawGrid();
 });
 
 map.on('pointermove', function (event) {
-	if (drawMode == 'GeoHash') console.log(geohash.encode(toLonLat(event.coordinate)[1], toLonLat(event.coordinate)[0], 1));
-	else if(drawMode == 'Slippy') getSlippyTileID(event.coordinate);
+	logCoordinates(event.coordinate);
 });
